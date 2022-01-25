50b89f64ad4817a0526980806709df4077f4356f
Randhir Kumar Singh <heyrandhirs@gmail.com>
2022-01-25 22:15:51 +0530

    initial commit

---
+++ data.ts
@@ -0,0 +1,86 @@
+export const data = [
+    {
+        "id": 68645921476505215000,
+        "firstname": "Harmonia",
+        "city": "Sabha",
+        "email ": "Harmonia@gmail.com",
+        "subject": "Harmonia from Sabha"
+    },
+    {
+        "id": 48246465455961850000,
+        "firstname": "Arlina",
+        "city": "Liverpool",
+        "email ": "Arlina@gmail.com",
+        "subject": "Arlina from Liverpool"
+    },
+    {
+        "id": 63067658173468210000,
+        "firstname": "Steffane",
+        "city": "Santiago de Cuba",
+        "email ": "Steffane@gmail.com",
+        "subject": "Steffane from Santiago de Cuba"
+    },
+    {
+        "id": 60294857705321650000,
+        "firstname": "Asia",
+        "city": "Malang",
+        "email ": "Asia@gmail.com",
+        "subject": "Asia from Malang"
+    },
+    {
+        "id": 67152629225136824000,
+        "firstname": "Nicoli",
+        "city": "Hanoi",
+        "email ": "Nicoli@gmail.com",
+        "subject": "Nicoli from Hanoi"
+    },
+    {
+        "id": 70112180521196900000,
+        "firstname": "Hayley",
+        "city": "Vancouver",
+        "email ": "Hayley@gmail.com",
+        "subject": "Hayley from Vancouver"
+    },
+    {
+        "id": 51955765174473335000,
+        "firstname": "Blake",
+        "city": "Turin",
+        "email ": "Blake@gmail.com",
+        "subject": "Blake from Turin"
+    },
+    {
+        "id": 25123839115112804000,
+        "firstname": "Beth",
+        "city": "Kharkiv",
+        "email ": "Beth@gmail.com",
+        "subject": "Beth from Kharkiv"
+    },
+    {
+        "id": 20699748393825296000,
+        "firstname": "Jorry",
+        "city": "Medellín",
+        "email ": "Jorry@gmail.com",
+        "subject": "Jorry from Medellín"
+    },
+    {
+        "id": 27228840289337238000,
+        "firstname": "Misha",
+        "city": "Ahmedabad",
+        "email ": "Misha@gmail.com",
+        "subject": "Misha from Ahmedabad"
+    },
+    {
+        "id": 50869184871488040000,
+        "firstname": "Concettina",
+        "city": "Malé",
+        "email ": "Concettina@gmail.com",
+        "subject": "Concettina from Malé"
+    },
+    {
+        "id": 49269918724462810000,
+        "firstname": "Britte",
+        "city": "Edinburgh",
+        "email ": "Britte@gmail.com",
+        "subject": "Britte from Edinburgh"
+    }
+]
